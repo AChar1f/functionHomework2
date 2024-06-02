488e0d7b1b12b89344a228f299b7f42f814d1e53
AChar1f <acharif41@gmail.com>
2024-06-02 17:44:17 +0200

    updated

--- script/code.js
+++ script/code.js
@@ -1,3 +1,19 @@
+// Palindrome checker function
+function isPalindrome(string) {
+    let reverseString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseString += string[i];
+    }
+    if (reverseString == string) {
+        return ("Is a Palindrome")
+    } else {
+        return ("Is Not a Palindrome")
+    }
+}
+console.log(isPalindrome("racecar"))
+console.log(isPalindrome("Bonjour"))
+
+//Exponent function
 function pow(base, exponent) {
     if(exponent == 0) {
         return 1
